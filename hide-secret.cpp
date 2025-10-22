f5b4902c1760f6a8fe3d9f4867874b4c63989af2
LegendaV <115637903+LegendaV@users.noreply.github.com>
2025-10-22 23:38:25 +0500

    Create hide-secret.cpp

---
+++ hide-secret.cpp
@@ -0,0 +1,28 @@
+void check_and_change(char* const text, const char* const secret, const int shift)
+{
+    int i = 0;
+    while (secret[i] != '\0')
+    {
+        if (secret[i] != text[i + shift])
+        {
+            return;
+        }
+        i++;
+    }
+
+    i = 0;
+    while (secret[i] != '\0')
+    {
+        text[i + shift] = '*';
+        i++;
+    }
+}
+
+void hide_secret(char* const text, const char* const secret)
+{
+    int i = 0;
+    while (text[i] != '\0')
+    {
+        check_and_change(text, secret, ++i);
+    }
+}
